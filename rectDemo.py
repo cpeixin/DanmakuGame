aa047f3fbec7b2da30456e9c939dbd5abb554a4c
cpeixin <421391491@qq.com>
2022-10-17 22:24:10 +0800

    添加人数占比Bar

--- rectDemo.py
+++ rectDemo.py
@@ -22,8 +22,26 @@ pygame.display.set_caption("Python Demo")
 
 clock = pygame.time.Clock()
 
-rect1 = pygame.Rect(0, 0, 100, 50)
-rect2 = pygame.Rect(50, 50, 200, 200)
+total_width = 300
+
+r_people = 0
+b_people = 0
+
+
+r_left = 20
+r_top = 20
+r_width = (r_people / (r_people + b_people + 1)) * total_width
+r_height = 20
+
+b_left = r_left+r_width
+b_top = 20
+b_width = (b_people / (r_people + b_people + 1)) * total_width
+b_height = 20
+               # left, top, width, height
+rect1 = pygame.Rect(r_left, r_top, r_width, r_height)
+# rect2 = pygame.Rect(50, 50, 200, 200)
+rect2 = pygame.Rect(b_left, b_top, b_width, b_height)
+
 
 while True:
     for event in pygame.event.get():
@@ -35,7 +53,7 @@ while True:
 
     pygame.draw.rect(screen, (255, 0, 0), rect1)
     pygame.draw.rect(screen, (0, 255, 0), rect2)
-    pygame.draw.rect(screen, (0, 0, 255), rect1.fit(rect2))
+    # pygame.draw.rect(screen, (0, 0, 255), rect1.fit(rect2))
 
     pygame.display.flip()
 
